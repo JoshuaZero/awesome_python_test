e26b21f757e8b70d37a024dd2ba8a749d8656cb1
shuailiu <shuailiu@aibee.com>
2020-07-01 14:35:13 +0800

    MOD:update file

--- src/awesome_python/magic_method.py
+++ src/awesome_python/magic_method.py
@@ -19,11 +19,28 @@ class FrenchDeck:
 
     def __getitem__(self,position):
         return self._cards[position]
+    
+    
+suits_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    print("rank_value {}".format(rank_value))
+    print("lenth: {}".format(len(suits_values)))
+    print("current card suit: {}".format(suits_values[card.suit]))
+    return rank_value*len(suits_values) + suits_values[card.suit]
 
+   
 if __name__ == "__main__":
     beer_card = Card('7','diamonds')
     print(beer_card)
     fd_op = FrenchDeck()
     print(len(fd_op))
     print(choice(fd_op))
-    
+    for k in fd_op:
+        print(k)
+    print("-------------------------\n")    
+    for f in reversed(fd_op):
+        print(f)
+    print("=======\n")
+    for i in sorted(fd_op, key=spades_high):
+        print(i)
